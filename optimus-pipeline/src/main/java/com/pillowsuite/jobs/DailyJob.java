d8aa148b2d41359c2af315045383580df570bdb3
sanchjdev <sanchjdev@protonmail.com>
2025-05-04 15:05:51 -0700

    Added shade plugin to optimus poml

--- optimus-pipeline/src/main/java/com/pillowsuite/jobs/DailyJob.java
+++ optimus-pipeline/src/main/java/com/pillowsuite/jobs/DailyJob.java
@@ -1,9 +1,9 @@
-package com.pillowsuite.service;
+package com.pillowsuite.jobs;
 
+import com.pillowsuite.service.DataTransferProcess;
+import com.pillowsuite.service.MarketDateInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.time.LocalDate;
 
 public class DailyJob {
 
