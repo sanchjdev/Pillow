939e56e5818f97daf3cdff4a8b6940a2344a379a
sanchjdev <sanchjdev@protonmail.com>
2025-05-05 23:20:21 -0700

    consumer classes

--- rodimus-pipeline/src/main/java/com/pillowsuite/service/AllTickersConsumer.java
+++ rodimus-pipeline/src/main/java/com/pillowsuite/service/AllTickersConsumer.java
@@ -4,18 +4,17 @@ import com.pillowsuite.shared.messaging.enums.RabbitMQueue;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DeliverCallback;
 
-public class TopMoversConsumer extends RabbitMqConsumer {
+public class AllTickersConsumer extends RabbitMqConsumer {
 
-    public TopMoversConsumer() throws Exception {
-        rabbitQueue = RabbitMQueue.TOP_MOVERS;
+    public AllTickersConsumer() throws Exception{
+        rabbitQueue = RabbitMQueue.ALL_TICKERS;
     }
 
     @Override
-    protected DeliverCallback createDeliverCallback(Channel channel) {
+    protected DeliverCallback createDeliverCallback(Channel channel){
         return (consumerTag, delivery) -> {
             message = new String(delivery.getBody(), "UTF-8");
             logger.info("RECEIVED MESSAGE FROM " + rabbitQueue.name() + ".");
         };
-
     }
 }
